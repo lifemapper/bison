026c33e49a438ac5a1f3dbbca589aa81e41fd535
Aimee Stewart <astewart@badenov.nhm.ku.edu>
2018-07-10 16:58:05 -0500

    testing

--- src/recordRead.py
+++ src/recordRead.py
@@ -4,17 +4,42 @@ import json
 import csv
 import xml.etree.ElementTree as ET
 
+ns = {'tdwg': 'http://rs.tdwg.org/dwc/text/'}
+startCh = '/'
+# nslen = len(ns)
+pth = '/tank/data/input/bison/territories'
+interpBase = 'occurrence.txt'
+verbatimBase = 'verbatim.txt'
+fieldmetaBase = 'meta.xml'
+DELIMITER = '\t'
 
-interpFname = 'occurrence.txt'
-verbatimFname = 'verbatim.txt'
-fieldmetaFname = 'meta.xml'
+interpFname = os.path.join(pth, interpBase)
+verbatimFname = os.path.join(pth, verbatimBase)
+fieldmetaFname = '/tank/data/input/bison/us/meta.xml'
 
-tree = ET.parse(fieldmetaFname)
-root = tree.getroot()
-for child in root:
-   print child.tag, child.attrib
-
+# .............................................................................
+def getFileMeta():
+   fields = {interpBase: {},
+             verbatimBase: {} }
+   tree = ET.parse(fieldmetaFname)
+   root = tree.getroot()
+   for child in root:
+      fls = child.find('tdwg:files', ns)
+      dloc = fls.find('tdwg:location', ns)
+      currmeta = dloc.text
+      if currmeta in fields.keys(): 
+         flds = child.findall('tdwg:field')
+         for fld in flds:
+            idx = fld.get('index')
+            temp = fld.get('term')
+            term = temp[temp.rfind(startCh)+1:]
+            fields[currmeta][term] = idx
+   return fields
+      
+# .............................................................................
+      
 with open(interpFname, 'r') as interpF:
+   occreader = csv.reader(interpF, delimiter=DELIMITER)
    for i, line in enumerate(interpF):
       print line
       if i == 4:
