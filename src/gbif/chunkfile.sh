fba46f6ac6e682bec7f31ea6577149e9739d1c25
Aimee Stewart <astewart@badenov.nhm.ku.edu>
2018-08-03 22:01:26 -0500

    update to run process script

--- src/gbif/chunkfile.sh
+++ src/gbif/chunkfile.sh
@@ -1,14 +1,23 @@
 pth=terr
 base=occurrence
+outbase=outBison
 inc=1000000
+PROCESS_CSV=/state/partition1/workspace/bison/src/gbif/gbif2bison.py
 
 start=1
 for start in {1000000..46000000..1000000}; do 
     stop=$((start+inc))
     if [ $start -gt 1 ]; then
-        outfile=${base}_lines_$start-$stop.csv
-        if [ -s $outfile ]; then
-        	echo $outfile exists
+        infile=${base}_lines_$start-$stop.csv
+        outfile=${outbase}_lines_$start-$stop.csv
+        if [ -s $infile ]; then
+        	echo $infile exists
+		if [ -s $outfile ]; then
+			echo $outfile exists
+		else
+			echo process me!!
+			#time python $PROCESS_CSV --start_line=$start --stop_line=$stop
+		fi
         else
         	echo $outfile is empty or does not exist
         	sed -e "1,${start}d;${stop}q" $base.txt > $outfile
