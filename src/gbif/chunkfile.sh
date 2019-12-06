447f7c4ee3510ce313973b5a0d9f4c2d9805bd79
zzeppozz <zzeppozz@gmail.com>
2019-12-06 17:52:23 -0600

    bugfix

--- src/gbif/chunkfile.sh
+++ src/gbif/chunkfile.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #
-# This script 
-# sed -e "1,${start}d;${idx}q" $pth/$base.txt > $pth/${base}_lines_$start-$stop.csv
-
-
-#time python /state/partition1/workspace/bison/src/gbif/gbif2bison.py \
-#     --start_line=$start --stop_line=$stop
-
+# linecount of /tank/data/bison/2019/Terr/occurrence.txt = 72890559
+# bash /state/partition1/git/bison/src/gbif/chunkfile.sh Terr 72890559 10000000
+#
+# Example command
 # sed -e '1,5000d;10000q' occurrence.txt > occurrence_lines_5000-10000.csv
 
 
 usage () 
 {
-    echo "Usage: $0 <SUBDIR>  <LINECOUNT_PER_CHUNK>"
+    echo "Usage: $0 <SUBDIR>  <TOTAL_LINECOUNT>  <LINECOUNT_PER_CHUNK>"
     echo "This script is run on a large GBIF occurrence download named"
     echo "occurrence.txt in the <SUBDIR> subdirectory of /tank/data/bison/2019/"
     echo "  and chunk it into files of LINECOUNT_PER_CHUNK lines or less"
@@ -24,8 +21,7 @@ set_defaults() {
     dataname=occurrence
     pth=/tank/data/bison/2019
     SUBDIR=$1
-    LINECOUNT=$2
-    THISPTH=$pth/$SUBDIR/
+    THISPTH=$pth/$SUBDIR
     INFILE=$THISPTH/$dataname.txt
     echo 'Input = ' $INFILE
 	
@@ -41,42 +37,50 @@ chunk_data() {
     stop=$2
     postfix=_lines_$start-$stop
 	
-    outfile=$THISPTH/$base$postfix.csv
+    outfile=$THISPTH/$dataname$postfix.csv
+    echo 'Output = ' $outfile
+    
     if [ -s $outfile ]; then
     	TimeStamp "${outfile} exists"
     else
-    	TimeStamp "${outfile} is empty or does not exist"  
-    	TimeStamp "chunk into ${outfile}" >> $LOG
-    	sed -e "1,${start}d;${stop}q" $INFILE > $outfile
-    	TimeStamp ""
+     	TimeStamp "chunk into ${outfile}" >> $LOG
+     	sedstr=`echo "1,${start}d;${stop}q"`
+     	echo 'sed string = '  $sedstr
+     	sed -e $sedstr $INFILE > $outfile
+        echo ''
+        echo '' >> $LOG
     fi
 }
 
 ### Log progress
 TimeStamp () {
+    echo $1 `/bin/date`
     echo $1 `/bin/date` >> $LOG
 }
 
 
 ################################# Main #################################
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     usage
     exit 0
 fi 
 
+set_defaults $1
+linecount=$2
+inc=$3
 
-set_defaults $1 $2
 TimeStamp "# Start"
+start=1
 
-c=1
-while [[ $c -le $count ]]
+while [[ $start -lt linecount ]]
 do 
 	stop=$((start+inc))
+	if [[ $stop -ge linecount ]]; then
+	    stop=$linecount
+	fi
+	echo 'Start/stop = '  $start  $stop
 	chunk_data $start $stop
 	start=$stop
-
-	TimeStamp "Loop $c"
-	let c=c+1
 done
 TimeStamp "# End"
 ################################# Main #################################
