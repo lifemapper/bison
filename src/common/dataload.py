c6daaf567575c3c78a797787db0e309f203b60e1
zzeppozz <zzeppozz@gmail.com>
2020-04-12 12:35:54 -0500

    indentation fix; streamline

--- src/common/dataload.py
+++ src/common/dataload.py
@@ -62,24 +62,21 @@ def get_header(filename):
 # .............................................................................
 def _get_process_count():
     return cpu_count() - 2
-        
 
 # .............................................................................
-def find_chunk_files(big_csv_filename):
+def _find_chunk_files(big_csv_filename):
     """ Finds multiple smaller input csv files from a large input csv file, 
-    if they exist, and return these filenames, paired with output filenames for the results of 
-    processing these files. """
+    if they exist, and return these filenames, paired with output filenames 
+    for the results of processing these files. """
     cpus2use = _get_process_count()
     in_base_filename, ext = os.path.splitext(big_csv_filename)
     pth, basename = os.path.split(in_base_filename)
     # We know filename starts with 'step' followed by 1 char integer
     nextstep = str(int(basename[4]) + 1)
     out_base_filename = os.path.join(pth, basename[:4] + nextstep + basename[5:])
-    # Do not count header
     total_lines = get_line_count(big_csv_filename) - 1
     chunk_size = int(total_lines / cpus2use)
     
-    header = None
     csv_filename_pairs = []
     start = 1
     stop = chunk_size
@@ -88,31 +85,28 @@ def find_chunk_files(big_csv_filename):
         out_filename =  '{}_{}-{}{}'.format(out_base_filename, start, stop, ext)
         if os.path.exists(in_filename):
             csv_filename_pairs.append((in_filename, out_filename))
-            small_count = get_line_count(in_filename)
         else:
-            csv_filename_pairs = None
+            # Return basenames if files are not present
+            csv_filename_pairs = [(in_base_filename, out_base_filename)]
             print('Missing file {}'.format(in_filename))
             break
         start = stop + 1
         stop = start + chunk_size - 1
-        
-    if csv_filename_pairs is not None:
-        header = get_header(big_csv_filename)
-    return csv_filename_pairs, header
+    return csv_filename_pairs, chunk_size
 
 # .............................................................................
-def make_chunk_files(big_csv_filename):
+def get_chunk_files(big_csv_filename):
     """ Creates multiple smaller input csv files from a large input csv file, and 
     return these filenames, paired with output filenames for the results of 
     processing these files. """
-    cpus2use = _get_process_count()
-    in_base_filename, ext = os.path.splitext(big_csv_filename)
-    pth, basename = os.path.split(in_base_filename)
-    # We know filename starts with 'step' followed by 1 char integer
-    nextstep = str(int(basename[4]) + 1)
-    out_base_filename = os.path.join(pth, basename[:4] + nextstep + basename[5:])
-    total_lines = get_line_count(big_csv_filename)
-    chunk_size = int(total_lines / cpus2use)
+    csv_filename_pairs, chunk_size = _find_chunk_files(big_csv_filename)
+    # First pair is existing files OR basenames
+    if os.path.exists(csv_filename_pairs[0][0]):
+        header = get_header(big_csv_filename)
+        return csv_filename_pairs, header
+    else:
+        in_base_filename = csv_filename_pairs[0][0]
+        out_base_filename = csv_filename_pairs[0][1]
     
     csv_filename_pairs = []
     try:
@@ -124,8 +118,8 @@ def make_chunk_files(big_csv_filename):
             # Reset vars for next chunk
             start = curr_recno
             stop = start + chunk_size - 1
-            in_filename = '{}_{}-{}{}'.format(in_base_filename, start, stop, ext)
-            out_filename =  '{}_{}-{}{}'.format(out_base_filename, start, stop, ext)
+            in_filename = '{}_{}-{}.csv'.format(in_base_filename, start, stop)
+            out_filename =  '{}_{}-{}.csv'.format(out_base_filename, start, stop)
             csv_filename_pairs.append((in_filename, out_filename))
             try:
                 # Start writing the smaller file
@@ -155,35 +149,40 @@ def make_chunk_files(big_csv_filename):
 def step_parallel(in_csv_filename, terrestrial_data, marine_data, ancillary_path,
                   out_csv_filename):
     """Main method for parallel execution of geo-referencing script"""
-    csv_filename_pairs, header = find_chunk_files(in_csv_filename)
-    if csv_filename_pairs is None:
-        csv_filename_pairs, header = make_chunk_files(in_csv_filename)
+    csv_filename_pairs, header = get_chunk_files(in_csv_filename)
     
-    in_csv_fn, out_csv_fn = csv_filename_pairs[0]
-#     in_csv_fn = '/tank/data/bison/2019/Terr/tmp/step_painful_in.csv'
-#     out_csv_fn = '/tank/data/bison/2019/Terr/tmp/step_painful_uscanbound_out.csv'
-    intersect_csv_and_shapefiles(in_csv_fn, terrestrial_data, 
-                marine_data, ancillary_path, out_csv_fn)
-
-#     with ProcessPoolExecutor() as executor:
-#         for in_csv_fn, out_csv_fn in csv_filename_pairs:
-#             executor.submit(
-#                 intersect_csv_and_shapefiles, in_csv_fn, terrestrial_data, 
+#     in_csv_fn, out_csv_fn = csv_filename_pairs[0]
+#     in_csv_fn = '/tank/data/bison/2019/Terr/tmp/step3_occurrence_lines_30000001-40000001_8000001-9000000.csv'
+#     out_csv_fn = '/tank/data/bison/2019/Terr/tmp/step4_occurrence_lines_30000001-40000001_8000001-9000000.csv'
+#     intersect_csv_and_shapefiles(in_csv_fn, terrestrial_data, 
 #                 marine_data, ancillary_path, out_csv_fn)
-#           
-#     try:
-#         outf = open(out_csv_filename, 'w', encoding='utf-8')
-#         outf.write('{}\n'.format(header))
-#         for _, small_csv_fn in csv_filename_pairs:
-#             try:
-#                 for line in open(small_csv_fn, 'r', encoding='utf-8'):
-#                     outf.write('{}\n'.format(line))
-#             except Exception as inner_err:
-#                 print('Failed to write {} to merged file; {}'.format(small_csv_fn, inner_err))
-#     except Exception as outer_err:
-#         print('Failed to write to {}; {}'.format(out_csv_filename, outer_err))
-#     finally:
-#         outf.close()
+ 
+    with ProcessPoolExecutor() as executor:
+        for in_csv_fn, out_csv_fn in csv_filename_pairs:
+            executor.submit(
+                intersect_csv_and_shapefiles, in_csv_fn, terrestrial_data, 
+                marine_data, ancillary_path, out_csv_fn)
+    
+    try:
+        outf = open(out_csv_filename, 'w', encoding='utf-8')
+        outf.write('{}\n'.format(header))
+        for _, small_csv_fn in csv_filename_pairs:
+            lineno = 0
+            try:
+                for line in open(small_csv_fn, 'r', encoding='utf-8'):
+                    # Skip header in each file
+                    if lineno == 0:
+                        pass
+                    else:
+                        outf.write('{}'.format(line))
+                    lineno += 1
+            except Exception as inner_err:
+                print('Failed to write {} to merged file; {}'.format(small_csv_fn, inner_err))
+    except Exception as outer_err:
+        print('Failed to write to {}; {}'.format(out_csv_filename, outer_err))
+    finally:
+        outf.close()
+        
 # ...............................................
 if __name__ == '__main__':
     import argparse
