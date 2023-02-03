38f34f5b3ef9814b11cfd6826a300bd8162a9355
zzeppozz <zzeppozz@gmail.com>
2023-02-03 16:52:41 -0600

    test annotation result count

--- tests/tools/test_annotate_riis.py
+++ tests/tools/test_annotate_riis.py
@@ -21,14 +21,14 @@ class Test_annotate_riis:
         nnsl = NNSL(fn_args["_test_small_riis_filename"], logger, is_annotated=False)
         # Read original species data
         nnsl.read_riis()
-        assert len(nnsl.by_taxon) == fn_args["_test_riis_name_count"]
-        assert len(nnsl.by_riis_id) == fn_args["_test_riis_record_count"]
+        assert len(nnsl.by_taxon) == fn_args["_test_small_riis_name_count"]
+        assert len(nnsl.by_riis_id) == fn_args["_test_small_riis_record_count"]
         assert (
-            count_lines(fn_args["riis_filename"])
-            == fn_args["_test_riis_record_count"] + 1)
+            count_lines(fn_args["_test_small_riis_filename"])
+            == fn_args["_test_small_riis_record_count"] + 1)
 
     # .....................................
-    def test_read_full_riis(self):
+    def test_read_original_riis(self):
         """Test reading an original RIIS file by checking counts."""
         fn_args = get_test_parameters(script_name)
         logger = Logger(script_name, None)
@@ -41,20 +41,6 @@ class Test_annotate_riis:
             count_lines(fn_args["riis_filename"])
             == fn_args["_test_riis_record_count"] + 1)
 
-    # # .....................................
-    # def test_read_small_riis(self):
-    #     """Test reading an original RIIS file by checking counts."""
-    #     fn_args = get_test_parameters(script_name)
-    #     logger = Logger(script_name, None)
-    #     nnsl = NNSL(fn_args["_test_small_riis_filename"], logger, is_annotated=False)
-    #     # Read original species data
-    #     nnsl.read_riis()
-    #     assert len(nnsl.by_taxon) == fn_args["_test_small_riis_name_count"]
-    #     assert len(nnsl.by_riis_id) == fn_args["_test_small_riis_record_count"]
-    #     assert (
-    #         count_lines(fn_args["_test_small_riis_filename"])
-    #         == fn_args["_test_small_riis_record_count"] + 1)
-
     # .....................................
     def test_resolve_small_riis(self):
         """Test resolving taxa in a small RIIS file by checking counts."""
@@ -66,3 +52,17 @@ class Test_annotate_riis:
                 fn_args["_test_small_annotated_riis_filename"], overwrite=True)
         assert name_count == fn_args["_test_small_riis_gbiftaxa_count"]
         assert rec_count == fn_args["_test_small_riis_record_count"]
+
+    # .....................................
+    def test_read_annotated_riis(self):
+        """Test reading an original RIIS file by checking counts."""
+        fn_args = get_test_parameters(script_name)
+        logger = Logger(script_name, None)
+        nnsl = NNSL(fn_args["_test_annotated_riis_filename"], logger, is_annotated=True)
+        # Read original species data
+        nnsl.read_riis()
+        assert len(nnsl.by_taxon) == fn_args["_test_annotated_riis_name_count"]
+        assert len(nnsl.by_riis_id) == fn_args["_test_annotated_riis_record_count"]
+        assert (
+            count_lines(fn_args["_test_annotated_riis_filename"])
+            == fn_args["_test_riis_record_count"] + 1)
