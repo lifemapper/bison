7ead11b8377faa7ec9775f18e28c880a0feca1cc
zzeppozz <zzeppozz@gmail.com>
2023-03-06 17:37:03 -0600

    PAM creation, unfinished

--- bison/tools/encode_points_to_pam.py
+++ bison/tools/encode_points_to_pam.py
@@ -2,22 +2,14 @@
 import glob
 import json
 import os
-from datetime import datetime
 
 from bison.common.constants import CONFIG_PARAM
 from bison.tools._config_parser import get_common_arguments
 
-from lmpy.data_preparation.occurrence_splitter import (
-    DEFAULT_MAX_WRITERS, get_writer_key_from_fields_func, get_writer_filename_func,
-    OccurrenceSplitter)
-from lmpy.point import PointCsvReader
-
 script_name = os.path.splitext(os.path.basename(__file__))[0]
 DESCRIPTION = """\
-        Split one or more CSV files containing GBIF Occurrence records annotated with
-        geographic areas and determinations from the USGS Registry for
-        Introduced and Invasive Species (RIIS).  Split CSV files by any taxonomic field,
-        but preferably by `species`."""
+        Encode all species CSV files into presence-absence layers in a PAM."""
+
 # Options to be placed in a configuration file for the command
 PARAMETERS = {
     "required":
@@ -27,8 +19,8 @@ PARAMETERS = {
                     CONFIG_PARAM.TYPE: str,
                     CONFIG_PARAM.IS_INPUT_FILE: True,
                     CONFIG_PARAM.HELP:
-                        "Fullpath with filepattern of filenames containing raw GBIF "
-                        "occurrence records in CSV format"
+                        "Fullpath with filepattern of filenames, one per species, "
+                        "containing occurrence records in CSV format"
                 },
             "species_key":
                 {
@@ -48,17 +40,37 @@ PARAMETERS = {
                     CONFIG_PARAM.HELP:
                         "Fieldname for the latitude value in data."
                 },
-            "out_fields":
+            "min_x":
                 {
-                    CONFIG_PARAM.TYPE: list,
-                    CONFIG_PARAM.HELP:
-                        "Fieldnames for fields to write to the output files."
+                    CONFIG_PARAM.TYPE: float,
+                    CONFIG_PARAM.HELP: "Minimum longitude boundary for the output PAM."
                 },
-            "output_path":
+            "min_y":
+                {
+                    CONFIG_PARAM.TYPE: float,
+                    CONFIG_PARAM.HELP: "Minimum latitude boundary for the output PAM."
+                },
+            "max_x":
+                {
+                    CONFIG_PARAM.TYPE: float,
+                    CONFIG_PARAM.HELP: "Maximum longitude boundary for the output PAM."
+                },
+            "max_y":
+                {
+                    CONFIG_PARAM.TYPE: float,
+                    CONFIG_PARAM.HELP: "Maximum latitude boundary for the output PAM."
+                },
+            "resolution":
+                {
+                    CONFIG_PARAM.TYPE: float,
+                    CONFIG_PARAM.HELP:
+                        "Grid cell size in decimal degrees for the output PAM."
+                },
+            "out_filename":
                 {
                     CONFIG_PARAM.TYPE: str,
                     CONFIG_PARAM.IS_OUPUT_DIR: True,
-                    CONFIG_PARAM.HELP: "Destination directory for output data."
+                    CONFIG_PARAM.HELP: "Destination file for output PAM."
                 },
         },
     "optional":
@@ -79,51 +91,27 @@ PARAMETERS = {
 
 
 # .............................................................................
-def split_occurrence_files(
-        csv_filenames, species_key, x_key, y_key, out_fields, output_path, logger):
-    """Annotate GBIF records with geographic areas, and RIIS key and assessment.
+def build_grid(min_x, min_y, max_x, max_y, resolution, output_path, logger):
+    """Create a grid to structure the species layers.
 
     Args:
-        csv_filenames (list): full filenames containing GBIF data to merge and split by
-            species.
-        species_key (str): name of the field for grouping by species
-        x_key (str): name of the field containing longitude value.
-        y_key (str): name of the field containing latitude value.
-        out_fields (list): names of the fields to write to output files.
-        output_path: destination directory for output species CSV files.
+        min_x (float): minimum longitude boundary for the output PAM.
+        min_y (float): minimum latitude boundary for the output PAM.
+        max_x (float): maximum longitude boundary for the output PAM.
+        max_y (float): maximum latitude boundary for the output PAM.
+        resolution (float): resolution of gridcells for the output PAM>
+        output_path (str): destination directory for output species CSV files.
         logger (object): logger for writing relevant processing messages.
 
     Returns:
         report (dict): dictionary of metadata about the data and process.
     """
     report = {
-        "species_key": species_key,
-        "x_key": x_key,
-        "y_key": y_key,
+        "min_x": min_x,
+        "min_y": min_y,
+        "max_x": max_x,
+        "max_y": max_y
     }
-
-    # Establish functions for getting writer key and filename
-    writer_key_func = get_writer_key_from_fields_func(*tuple([species_key]))
-    writer_filename_func = get_writer_filename_func(output_path)
-
-    logger.log(f"Start Time : {datetime.now()}", refname=script_name)
-    # Initialize processor
-    with OccurrenceSplitter(
-        writer_key_func, writer_filename_func, write_fields=out_fields,
-        max_writers=DEFAULT_MAX_WRITERS, logger=logger
-    ) as occurrence_processor:
-        for csv_fn in csv_filenames:
-            reader = PointCsvReader(csv_fn, species_key, x_key, y_key)
-            # We will do no other wrangling on these data
-            wranglers = []
-            curr_report = occurrence_processor.process_reader(reader, wranglers)
-            report[csv_fn] = curr_report
-
-        logger.log(
-            f"Start Time: {datetime.now()}: Submit {csv_fn} for splitting by "
-            f"species to {output_path}", refname=script_name)
-
-    logger.log(f"End Time : {datetime.now()}", refname=script_name)
 
     return report
 
@@ -144,9 +132,9 @@ def cli():
     if config["csv_file_pattern"] is not None:
         csv_filenames = glob.glob(os.path.join(config["csv_file_pattern"]))
 
-    report = split_occurrence_files(
-        csv_filenames, config["species_key"], config["x_key"], config["y_key"],
-        config["out_fields"], config["output_path"], logger)
+    report = build_grid(
+        csv_filenames, config["x_min"], config["y_min"], config["x_max"],
+        config["x_max"], config["resolution"], config["output_path"], logger)
 
     # If the output report was requested, write it
     if report_filename:
