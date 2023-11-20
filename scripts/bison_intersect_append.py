b783f0517e2b2934e19eca973f0f338c6ddcb46f
Aimee Stewart <zzeppozz@gmail.com>
2023-11-20 17:18:04 -0600

    intersection step testing

---
+++ scripts/bison_intersect_append.py
@@ -0,0 +1,166 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from awsglue.job import Job
+import boto3
+from botocore.exceptions import ClientError
+from pyspark.context import SparkContext
+from sqlalchemy import create_engine, URL
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+
+REGION = "us-east-1"
+BISON_BUCKET = "s3://bison-321942852011-us-east-1/"
+SECRET_NAME = "admin_bison-db-test"
+
+
+# ----------------------------------------------------
+def get_secret(secret_name, region):
+    """Get a secret from the Secrets Manager for connection authentication.
+
+    Args:
+        secret_name: name of the secret to retrieve.
+        region: AWS region containint the secret.
+
+    Returns:
+        a dictionary containing the secret data.
+
+    Raises:
+        ClientError:  an AWS error in communication.
+    """
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region)
+    try:
+        secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise (e)
+    # Decrypts secret using the associated KMS key.
+    secret_str = secret_value_response["SecretString"]
+    return eval(secret_str)
+
+
+# ----------------------------------------------------
+def get_db_connection(db_name, secret):
+    """Create a psycopg2 connection to a PostgreSQL database.
+
+    Args:
+        db_name: Database to connect to.
+        secret: Dictionary containing AWS RDS database credentials.
+
+    Returns:
+        conn: psycopg2.connection to the database.
+
+    Raises:
+        Exception: on failure to connect to database.
+    """
+    try:
+        conn = psycopg2.connect(
+            host=secret["host"], database=db_name, user=secret["username"],
+            password=secret["password"]
+        )
+    except Exception as e:
+        raise Exception("Error connecting to PostgreSQL database:", e)
+    return conn
+
+
+# ----------------------------------------------------
+def get_db_engine(db_name, secret):
+    """Create a sqlalchemy engine to connect to a PostgreSQL database.
+
+    Args:
+        db_name: Database to connect to.
+        secret: Dictionary containing AWS RDS database credentials.
+
+    Returns:
+        engine: sqlalchemy.engine to the database.
+    """
+    drivername = "postgresql+psycopg2"
+    url_object = URL.create(
+        drivername,
+        username=secret["username"],
+        password=secret["password"],  # plain (unescaped) text
+        host=secret["host"],
+        database=db_name,
+    )
+    engine = create_engine(url_object)
+    return engine
+
+
+# ----------------------------------------------------
+def get_s3_dynframe(s3_bucket, s3_path):
+    full_path = f"{s3_bucket}/{s3_path}"
+    bison_points_dynf = glueContext.create_dynamic_frame.from_options(
+        format_options={},
+        format="parquet",
+        connection_type="s3",
+        connection_options={
+            "paths": [full_path],
+            "recurse": True
+        }
+    )
+    print(f"Read {full_path} with {full_path.count()} records into dynamic frame.")
+    bison_points_dynf.printSchema()
+    return bison_points_dynf
+
+
+# ----------------------------------------------------
+def read_rds(secret, db_name, schema, tbl_name):
+    # Load polygon table from Amazon RDS PostgreSQL
+    rds_url = f"jdbc:postgresql://{secret['host']}:{secret['port']}/{db_name}"
+    polygon_table = spark.read.format("jdbc") \
+        .option("url", rds_url) \
+        .option("dbtable", f"{schema}.{tbl_name}") \
+        .option("user", secret["username"]) \
+        .option("password", secret["password"]) \
+        .option("driver", "org.posgresql.Driver") \
+        .load()
+    return polygon_table
+
+
+# ----------------------------------------------------
+# ----------------------------------------------------
+# rds_db_name = "bison_db_test"
+rds_instance = "bison-db-test"
+rds_db_schema = "lmb"
+county_table = "county"
+state_fieldname = "STUSPS"
+
+# ----------------------------------------------------
+sc = SparkContext.getOrCreate()
+# spark = SparkSession(sc)
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+n = DT.datetime.now()
+datastr = f"{n.year}-{n.month}-01"
+input_s3_path = f"raw_data/gbif_{datastr}.parquet/"
+output_s3_path = f"out_data/bison_{datastr}.parquet/"
+
+secret = get_secret(SECRET_NAME, REGION)
+
+# Get dynamic frame of point records
+gbif_pt_recs = get_s3_dynframe(BISON_BUCKET, input_s3_path)
+
+# Get spatial table
+polygon_table = read_rds(secret, rds_instance, rds_db_schema, county_table)
+
+intersected_points = gbif_pt_recs.crossJoin(polygon_table).filter(
+    "ST_INTERSECTS("
+        "ST_POINT(point_records.decimal_longitude, point_records.decimal_latitude), "
+        "polygon_table.geometry")
+
+# Intersect points with polygons
+intersected_points = intersected_points.withColumn("georef_st", col("polygon_table.STUSPS"))
+job.commit()
--- scripts/glue_bison_subset_gbif.py
+++ scripts/glue_bison_subset_gbif.py
@@ -1,6 +1,5 @@
 import datetime as DT
 import sys
-from awsglue.transforms import Filter
 from awsglue.utils import getResolvedOptions
 from pyspark import SparkConf
 from pyspark.context import SparkContext
@@ -32,36 +31,34 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # # Small test 5000 record dataset
-# bison_s3_fullname = f"{bison_path}/raw_data/gbif_5k_{datastr}.parquet/"
-# gbif_full_data = glueContext.create_sample_dynamic_frame_from_options(
+# bison_s3_fullname = f"{bison_bucket}/raw_data/gbif_5k_{datastr}.parquet/"
+# gbif_dynf = glueContext.create_sample_dynamic_frame_from_options(
+#     num=5000,
+#     format="parquet",
 #     connection_type="s3",
 #     connection_options={
 #         "paths": [ gbif_s3_fullname ],
 #         "recurse": True
 #     },
-#     num=5000,
-#     format="parquet",
-#     transformation_ctx="S3bucket_node_gbif"
+#     # transformation_ctx="S3bucket_node_gbif"
 # )
+# print(f"Created 5k dynamic frame of GBIF {gbif_s3_fullname}.")
 
 # Full dataset
 gbif_dynf = glueContext.create_dynamic_frame.from_options(
     format_options={},
-    connection_type="s3",
     format="parquet",
+    connection_type="s3",
     connection_options={
         "paths": [gbif_s3_fullname],
-        "recurse": True,
-        # 'groupFiles': 'inPartition',
-        # 'groupSize': '1073741824'
-    },
-    # transformation_ctx="S3bucket_node_gbif",
+        "recurse": True
+    }
 )
-print(f"Read GBIF {gbif_s3_fullname} with {gbif_dynf.count()} records.")
+print(f"Read GBIF {gbif_s3_fullname} with {gbif_dynf.count()} records into dynamic frame.")
 
 gbif_filtered_dynf = gbif_dynf.filter(
     f=lambda x: x["countrycode"] == "US" and x["occurrencestatus"] == "PRESENT" and x["taxonrank"] in ["SPECIES", "SUBSPECIES", "VARIETY", "FORM", "INFRASPECIFIC_NAME", "INFRASUBSPECIFIC_NAME"])
-print(f"Filtered GBIF to dynamic frame with {gbif_filtered_dynf.count()} records.")
+print(f"Filtered GBIF to dynamic frame with {gbif_filtered_dynf.count()} records .")
 
 glueContext.write_dynamic_frame.from_options(
     frame=gbif_filtered_dynf,
@@ -74,38 +71,5 @@ glueContext.write_dynamic_frame.from_options(
     }
 )
 print(f"Wrote dynamic frame to {bison_s3_fullname}.")
-
-# # Create filtered DynamicFrame with custom lambda to filter records
-# gbif_subset = Filter.apply(
-#     frame = gbif_full_data,
-#     f=lambda row: (
-#         bool(bool(re.match("US", row["countrycode"]))
-#             and bool(re.match("PRESENT", row["occurrencestatus"]))
-#             and bool(
-#                 re.match("SPECIES", row["taxonrank"])
-#                 or bool(re.match("SUBSPECIES", row["taxonrank"]))
-#                 or bool(re.match("VARIETY", row["taxonrank"]))
-#                 or bool(re.match("FORM", row["taxonrank"]))
-#                 or bool(re.match("INFRASPECIFIC_NAME", row["taxonrank"]))
-#                 or bool(re.match("INFRASUBSPECIFIC_NAME", row["taxonrank"]))
-#                 )
-#             )
-#     ))
-
-
-# # Output to BISON S3 bucket
-# bison_full_data = glueContext.write_dynamic_frame.from_options(
-#     frame=gbif_subset,
-#     connection_type="s3",
-#     connection_options={
-#         "path": bison_s3_fullname,
-#     },
-#     format="parquet",
-#     format_options={
-#         "useGlueParquetWriter": True,
-#         "compression": "snappy"
-#     },
-#     transformation_ctx="S3bucket_node_bison",
-# )
 
 job.commit()
