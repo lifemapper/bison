6ce2af5fac0ddd28bcbefb1cce10d3acd4f2a558
Aimee Stewart <zzeppozz@gmail.com>
2023-11-22 10:07:00 -0600

    cleanup

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,8 +1,9 @@
 """Setup script for Glue Spark script using additional python libraries."""
 from setuptools import setup
 
-setup(name="pylibmodule",
-      version="0.1",
-      packages=[],
-      install_requires=["sqlalchemy==2.0.23", "pandas==0.25.3", "pymysql==0.9.3"]
-  )
+setup(
+    name="pylibmodule",
+    version="0.1",
+    packages=[],
+    install_requires=["sqlalchemy==2.0.23", "pandas==0.25.3", "pymysql==0.9.3"]
+)
