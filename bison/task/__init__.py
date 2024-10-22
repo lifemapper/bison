565953c079419ed66ddca4d9970c5ded3def363c
Aimee Stewart <zzeppozz@gmail.com>
2024-10-22 13:46:46 -0500

    prevent RuntimeError and import twice on calling as module

--- bison/task/__init__.py
+++ bison/task/__init__.py
@@ -1,16 +1,1 @@
-"""Tools to be called from a Docker container in an EC2 instance."""
-from . import annotate_riis
-from . import build_heatmap
-from . import build_summaries
-from . import test_summaries
-from . import test_task
-
-__all__ = [
-    "annotate_riis",
-    "build_heatmap",
-    "build_summaries",
-    "test_summaries",
-    "test_task"
-]
-
-__version__ = "2.0"
+"""Tools to be called from a Docker container in an EC2 instance; run with -m switch."""
