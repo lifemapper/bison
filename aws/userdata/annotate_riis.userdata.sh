5fa880824ebace5a604f3e0a7a980f2f303fa0c5
Aimee Stewart <zzeppozz@gmail.com>
2024-10-28 16:19:59 -0500

    add logging to AWS

--- aws/userdata/annotate_riis.userdata.sh
+++ aws/userdata/annotate_riis.userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt update
-sudo apt install docker.io
-sudo apt install docker-compose-v2
+sudo apt-get -y update
+sudo apt-get -y install docker.io
+sudo apt-get -y install docker-compose-v2
 git clone https://github.com/lifemapper/bison.git
 cd bison
 sudo docker compose -f compose.annotate_riis.yml up
