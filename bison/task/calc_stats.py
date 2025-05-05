be5204fb02dc3f8c2e34c9f75852a25cfd4e756a
Aimee Stewart <zzeppozz@gmail.com>
2025-05-05 17:18:21 -0500

    stats to summary dir

--- bison/task/calc_stats.py
+++ bison/task/calc_stats.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     stats_zip_filename = pam.compress_stats_to_file(local_path=TMP_PATH)
     stats_data_dict, stats_meta_dict, table_type, datestr = PAM.uncompress_zipped_data(
         stats_zip_filename)
-    stats_key = f"{S3_OUT_DIR}/{os.path.basename(stats_zip_filename)}"
+    stats_key = f"{S3_SUMMARY_DIR}/{os.path.basename(stats_zip_filename)}"
     _uri = s3.upload(stats_zip_filename, S3_BUCKET, stats_key, overwrite=overwrite)
 
 """
